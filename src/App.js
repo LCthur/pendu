1ee10af1eb61c436e010910a036152998065a147
Loic Thurre <loic.thurre@gmail.com>
2019-08-27 17:23:29 +0200

    counter and winning phrase are working. Now some style improvements

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react'
 import './App.css';
 import Letter from './Letter'
 
+
+
 class App extends Component {
 
   constructor(props) {
@@ -9,34 +11,56 @@ class App extends Component {
 
     this.state = {
       phrase: "pendu",
-      alphabet: ("abcdefghijklmnopqrstuvwxyz").split(""),
-      usedLetters: ""
+      firstHalfAlphabet: ("abcdefghijklm").split(""),
+      secondHalfAlphabet: ("nopqrstuvwxyz").split(""),
+      usedLetters: "",
+      count: 0,
     }
   }
 
   // besoin de re render à chaque clic donc on bind this en tranformant cette fonction en fat arrow func
   computeDisplay = (phrase, usedLetters) => {
     return phrase.replace(/\w/g,
-      (letter) => (usedLetters.includes(letter) ? letter : ' _ ')
+      (letter) => (usedLetters.includes(letter) ? ` ${letter} ` : ' _ ')
     )
   }
 
   handleLetterClick = (letter) => {
+    console.log(this.state.count)
     this.setState({
-      usedLetters: `${this.state.usedLetters}${letter}`
+      usedLetters: `${this.state.usedLetters}${letter}`,
+      count: this.state.count + 1
     })
+  }
+
+  won = (phrase) => {
+    if (!phrase.includes('_')){
+      return true
+    }
   }
 
   render() {
     return (
       <div>
         <div className="display">
-          {this.computeDisplay(this.state.phrase, this.state.usedLetters )}
+          <h1 className="title">Mot à trouver</h1>
+          <h2>{this.computeDisplay(this.state.phrase, this.state.usedLetters )}</h2>
+          <h3>Nombre de tentatives: {this.state.count}</h3>
         </div>
         <div className="alphabet">
-          {this.state.alphabet.map((letter) => 
+          <div>
+            {this.state.firstHalfAlphabet.map((letter) => 
               <Letter letter={letter} key={letter} onClick={this.handleLetterClick} />
             )}
+          </div>
+          <div>
+            {this.state.secondHalfAlphabet.map((letter) => 
+              <Letter letter={letter} key={letter} onClick={this.handleLetterClick} />
+            )}
+          </div>
+        </div>
+        <div>
+          <h1>{this.won(this.computeDisplay(this.state.phrase, this.state.usedLetters)) && "Bravo ! Vous avez gagné"}</h1>
         </div>
       </div>
     );
--- src/Letter.js
+++ src/Letter.js
@@ -2,9 +2,7 @@ import React from 'react'
 import './Letter.css'
 
 const Letter = ({ letter, selected, onClick }) => (
-    <div className= {`${selected}`} onClick={() => onClick(letter)}>
-        <button>{ letter }</button>
-    </div>
+    <button className= {`${selected}`} onClick={() => onClick(letter)}> { letter }</button>
 )
 
 export default Letter
